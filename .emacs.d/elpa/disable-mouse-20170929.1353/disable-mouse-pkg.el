ed5da846af97092f84e47e0e1b315ad34ebf7fc5
axelhallsenius <axelhallsenius@gmail.com>
2017-10-14 17:35:58 +0200

    making emacs vim. I am a horrible person

---
+++ .emacs.d/elpa/disable-mouse-20170929.1353/disable-mouse-pkg.el
@@ -0,0 +1,1 @@
+(define-package "disable-mouse" "20170929.1353" "Disable mouse commands globally" 'nil :commit "81639930bcaeedadbcc19728e91719afcac84613" :url "https://github.com/purcell/disable-mouse" :keywords '("mouse"))
---
+++ .emacs.d/elpa/epl-20150517.433/epl-autoloads.el
@@ -0,0 +1,15 @@
+;;; epl-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil nil ("epl.el") (22995 40019 805252 224000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; epl-autoloads.el ends here
