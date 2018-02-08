a2e7a4f3ba174aee7be99dd79a4aec8d3fefae57
haxxel <axelhallsenius@gmail.com>
2018-02-08 21:09:39 +0100

    removed emacs conf. neever use it.

--- .emacs.d/elpa/monitor-20161018.444/monitor-autoloads.el
+++
@@ -1,16 +0,0 @@
-;;; monitor-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil nil ("monitor-pkg.el" "monitor.el") (22994
-;;;;;;  19414 517263 524000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; monitor-autoloads.el ends here
