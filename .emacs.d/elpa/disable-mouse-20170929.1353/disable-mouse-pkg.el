494045e6adf0fcf0071c1bff117f3d385294a116
Axel Hallsenius <axelhallsenius@gmail.com>
2019-05-21 11:50:08 +0200

    cleaned up for shitbrick

--- .emacs.d/elpa/disable-mouse-20170929.1353/disable-mouse-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "disable-mouse" "20170929.1353" "Disable mouse commands globally" 'nil :commit "81639930bcaeedadbcc19728e91719afcac84613" :url "https://github.com/purcell/disable-mouse" :keywords '("mouse"))
--- .emacs.d/elpa/org-evil-20170529.741/org-evil-autoloads.el
+++
@@ -1,17 +0,0 @@
-;;; org-evil-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil nil ("org-evil-commands.el" "org-evil-core.el"
-;;;;;;  "org-evil-list.el" "org-evil-motion.el" "org-evil-pkg.el"
-;;;;;;  "org-evil-table.el" "org-evil.el") (22994 19418 451410 934000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; org-evil-autoloads.el ends here
